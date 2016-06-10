7a768a6b8441e72c900909bc1f9f618697e16f73
nzmi <ingvr@icloud.com>
2016-06-10 11:25:27 +0500

    tests works now. TODO: Test about vowels and consonants

--- test.rb
+++ test.rb
@@ -10,7 +10,8 @@ class TestNameGenerate < Minitest::Test
  def test_name_should_be_valid
   assert (@name.length >= 2 && @name.length <= 12), "Should be 2-12 characters"
   assert_instance_of String, @name, "Should be a String type"
-  assert @name.capitalize?, "Should be Capitalized"
+  assert @name.capitalize==@name, "Should be Capitalized"
+  assert @name =~ /^[A-Z][a-z]+$/, "Should contain only letters"
  end
 end
 
